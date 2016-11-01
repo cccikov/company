f8161680d8cbcaabfb6b4874f63cc9995e4595d6
cccikov <522978139@qq.com>
2016-11-01 11:40:13 +0800

    fuck

--- moblie/maimai/js/base.js
+++
@@ -1,8 +0,0 @@
-function baseFontSize(){
-    $("html").css("fontSize",$(window).width()/20+"px");
-    return ($(window).width()/20);
-}
-var rem = baseFontSize();
-$(window).on("resize",function(){
-    rem = baseFontSize();
-});
