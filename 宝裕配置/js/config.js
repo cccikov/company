f18e82d3d0825c369e4db4b1ae2f00159892a932
cccikov <522978139@qq.com>
2017-01-09 09:27:53 +0800

    宝裕配置-修复bug:模拟select就算选择了值,但是显示的文字还是没有变化

--- 宝裕配置/js/config.js
+++ 宝裕配置/js/config.js
@@ -15,12 +15,18 @@ $(function(){
             that.removeClass("active");
             $(window).off("click.select");
         }
+    }).each(function(item,key){
+        var that = $(key);
+        var html = that.find("p").html();
+        that.attr("data-html",html);
     });
     $(".select li").on("click",function(e){
         e.stopPropagation();
         var that = $(this);
         var thisVal = that.attr("data-val");
         var parent = that.parents(".select");
+        var html = parent.attr("data-html");
+        var p = parent.find("p");
         var val = (parent.attr("data-val") != undefined && parent.attr("data-val") != "") ? JSON.parse(parent.attr("data-val")) : [];
         if(!that.is(".active")){
             that.addClass("active");
@@ -28,6 +34,11 @@ $(function(){
         }else{
             val.splice(val.indexOf(thisVal),1);
             that.removeClass("active");
+        }
+        if(val.length!=0){
+            p.html("已选择");
+        }else{
+            p.html(html);
         }
         parent.attr("data-val",JSON.stringify(val));
     });
