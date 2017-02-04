721b1cf760b0f9f92844d14c5a6f0fc6b6962812
cccikov <522978139@qq.com>
2017-02-04 17:45:11 +0800

    添加报价-更新内容

--- pc/js/base.js
+++ pc/js/base.js
@@ -1,8 +1,24 @@
-function fixNum(num){
-    if(typeof(num)=="string"){
+function fixNum(num) {
+    if (typeof(num) == "string") {
         num = Number(num);
     }
     var numArr = num.toFixed(2).split(".");
     numArr[0] = Number(numArr[0]).toLocaleString();
     return numArr.join(".");
+}
+
+function formatDate(d) {
+    var now = new Date(d);
+    var year = now.getFullYear();
+    var month = now.getMonth() + 1;
+    month < 10 ? month = "0" + month : month = month;
+    var day = now.getDate();
+    day < 10 ? day = "0" + day : day = day;
+    var hour = now.getHours();
+    hour < 10 ? hour = "0" + hour : hour = hour;
+    var minute = now.getMinutes();
+    minute < 10 ? minute = "0" + minute : minute = minute;
+    var second = now.getSeconds();
+    second < 10 ? second = "0" + second : second = second;
+    return hour + ":" + minute;
 }
