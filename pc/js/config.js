98715457ae6ba08b33d27ef3339b328cd26415d1
ccc <522978139@qq.com>
2017-02-05 23:16:13 +0800

    添加报价-新建一个地图颜色配置文件；以及几张图片

---
+++ pc/js/config.js
@@ -0,0 +1,43 @@
+[{
+    "featureType": "land",
+    "elementType": "all",
+    "stylers": {
+        "color": "#fcf9f2"
+    }
+}, {
+    "featureType": "green",
+    "elementType": "all",
+    "stylers": {
+        "color": "#cfe7b4"
+    }
+}, {
+    "featureType": "manmade",
+    "elementType": "all",
+    "stylers": {
+        "color": "#f8efe2"
+    }
+}, {
+    "featureType": "local",
+    "elementType": "geometry.stroke",
+    "stylers": {
+        "color": "#f5e6d3"
+    }
+}, {
+    "featureType": "local",
+    "elementType": "geometry.fill",
+    "stylers": {
+        "color": "#ffffff"
+    }
+}, {
+    "featureType": "arterial",
+    "elementType": "geometry.fill",
+    "stylers": {
+        "color": "#fbefd5"
+    }
+}, {
+    "featureType": "arterial",
+    "elementType": "geometry.stroke",
+    "stylers": {
+        "color": "#f6d19e"
+    }
+}]
