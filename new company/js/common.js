2bbb3fc60a5637f7f4a764c45e4669502adb11c2
cccikov <522978139@qq.com>
2017-03-10 17:00:24 +0800

    着手产品

--- new company/js/common.js
+++ new company/js/common.js
@@ -1,4 +1,5 @@
-function ieVersion(){
+// 判断ie版本
+function ieVersion() {
     var reg = /msie\s(\d+)\.0/i;
     var agent = navigator.userAgent.toLowerCase();
     var bodyClass = document.body.className;
@@ -11,6 +12,23 @@ function ieVersion(){
         }
     };
 }
-$(function(){
+// iframe高度;
+function frameHeight(){
+    ifrMinH = $(window).height()-350-30-30-50;
+    if($("#ifr").size()>0){
+        $("#ifr").css({
+            "min-height":ifrMinH,
+            "opacity":"0"
+        });//最小高度
+    }
+    $("#ifr").on("load", function() {
+        var that = $(this).css("opacity","1");
+        var ifrH = $(window.frames['ifr'].document.body)[0].scrollHeight;//跨域会不行
+        that.height(ifrH);
+    });
+}
+
+$(function() {
     ieVersion();
+    frameHeight();
 });
